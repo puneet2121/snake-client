c215a5e8cac685e8ef4b6f23ff56043dab6056c0
puneet2121 <bedipuneet21@gmail.com>
2022-05-11 22:56:56 +0000

    using export

--- play.js
+++ play.js
@@ -1,21 +1,7 @@
-const { Console } = require("console");
-const net = require("net");
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host:'165.227.47.243',
-    port: 50541  // PORT number here,
-  });
-  
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on('data', (data) => {
-    console.log(data.toString());
-    conn.end();
-  });
-  return conn;
-  
-};
 
+
+const obj = require("./client")
+// establishes a connection with the game server
+
+obj.new
 console.log("Connecting ...");
-connect();
