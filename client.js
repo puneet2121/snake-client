224f45b84df3a7d27aa34474daf1fe0463396946
puneet2121 <bedipuneet21@gmail.com>
2022-05-11 22:56:33 +0000

    adding new file

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host:'165.227.47.243',
+    port: 50541  // PORT number here,
+  });
+  
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on('data', (data) => {
+    console.log(data.toString());
+    conn.end();
+  });
+  return conn;
+  
+};
+
+module.exports = {
+  new : connect()
+}
